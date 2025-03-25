8095ba8e1e8d17a92ccae5add1e7153eddf71a25
zavodshowdev <zavodshowdev@gmail.com>
2025-03-25 05:44:12 -0400

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,28 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
+    // config.module.rules.push({
+    //   test: /\.css$/, // Target CSS files
+    //   include: [
+    //     /node_modules\/react-datepicker/, // Include react-datepicker styles
+    //     /node_modules\/swiper/, // Include swiper styles
+    //   ],
+    //   use: [
+    //     "style-loader", // Inject styles into the DOM
+    //     "css-loader", // Resolve @import and url() in CSS
+    //     "postcss-loader", // Optional but useful for advanced CSS features
+    //   ],
+    // });
+
     config.module.rules.push({
-      test: /\.pdf$/,
-      type: "asset/resource",
+      test: /\.pdf$/, // Match .pdf files
+      type: "asset/resource", // Handle PDFs as static assets
     });
 
     config.module.rules.push({
-      test: /\.(mp4|webm|ogg|avi|mkv)$/,
-      type: "asset/resource",
+      test: /\.(mp4|webm|ogg|avi|mkv)$/, // Match video formats
+      type: "asset/resource", // Handle videos as static assets
     });
 
     return config;
@@ -26,9 +38,9 @@ const nextConfig = {
       },
       {
         protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/uploads/**",
+        hostname: "localhost", // Localhost configuration
+        port: "8000", // Port for local development
+        pathname: "/storage/uploads/**", // Pattern for image paths
       },
     ],
   },
