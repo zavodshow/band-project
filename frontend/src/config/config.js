65243ccdccc92de9877b5dfbb2c0208f12ce01ad
zavodshowdev <zavodshowdev@gmail.com>
2025-03-27 18:48:41 -0400

    fix: ssl backend2

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,3 @@
-const endpoint = process.env.NEXT_PUBLIC_API_URL || "https://109.196.100.122:8000";
+const endpoint = process.env.NEXT_PUBLIC_API_URL || "https://zavodshow.ru";
 
 export default endpoint;
